2a7c4186f3c194342e91decaa89c866fab7a517b
unknown <soppongaddo@gmail.com>
2023-02-16 16:30:30 +0000

    fixed tests folder structure

--- test/test_battery.py
+++ test/test_battery.py
@@ -7,22 +7,21 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-from carfactory  import CarFactory as factory
 from battery import spindlerbattery, nubbinbattery
-from engine import capulet_engine, sternman_engine, willoughby_engine
 
 class TestSpindler(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year = current_date.year - 1)
+        last_service_date = current_date.replace(year = current_date.year - 3)
         car_battery = spindlerbattery.SpindlerBattery(last_service_date, current_date)
         self.assertTrue(car_battery.needs_service())
     
     
-#     def test_battery_should_not_be_serviced(last_service_date, current_date):
-#         current_date = datetime.today().date()
-#         last_service_date = current_date.replace(year=current_date.year - 1)
-#         pass
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+        car_battery = spindlerbattery.SpindlerBattery(last_service_date, current_date)
+        self.assertFalse(car_battery.needs_service())
     
     
     
@@ -33,20 +32,18 @@ class TestNubbin(unittest.TestCase):
         car_battery = nubbinbattery.NubbinBattery(last_service_date, current_date)
         self.assertTrue(car_battery.needs_service())
     
+
     
-    
-#     def battery_should_not_be_serviced(last_service_date, current_date):
-#         current_date = datetime.today().date()
-#         last_service_date = current_date.replace(year=current_date.year - 1)
-#         pass
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+        car_battery = spindlerbattery.SpindlerBattery(last_service_date, current_date)
+        self.assertFalse(car_battery.needs_service())
     
     
     
 
 
-
-if __name__ == '__main__':
-    unittest.main()
 
 if __name__ == '__main__':
     unittest.main()
