f93bb608bbfac43e4701946a27bc4bc8e0093856
unknown <soppongaddo@gmail.com>
2023-02-22 18:15:12 +0000

    implement TDD

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 from serviceable import Serviceable
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tyre):
         self.engine = engine
         self.battery = battery
+        self.tyre = tyre
         
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tyre.needs_service()
