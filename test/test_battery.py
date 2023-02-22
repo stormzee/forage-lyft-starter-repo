19a3b70fc827a4a28d409479d6122c4b2670c9f8
unknown <soppongaddo@gmail.com>
2023-02-22 17:51:38 +0000

    implement TDD

--- test/test_battery.py
+++ test/test_battery.py
@@ -12,7 +12,7 @@ from battery import spindlerbattery, nubbinbattery
 class TestSpindler(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year = current_date.year - 3)
+        last_service_date = current_date.replace(year = current_date.year - 4)
         car_battery = spindlerbattery.SpindlerBattery(last_service_date, current_date)
         self.assertTrue(car_battery.needs_service())
     
--- test/test_tyre.py
+++ test/test_tyre.py
@@ -1,20 +1,42 @@
 import unittest
 import os
 import sys
-from 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
+from tyre import Carrigan_tyre, Octoprime_tyre
 
-class TestTyre(unittest.TestCase):
-    def test_tyre_needs_service(self):
-        # create a tyre_type class
-        # tyre needs servicing if the array consist of a value greater than 0.4
-        
-        tyre_array = [0, 0.2, 0.03, 0.46]
-        tyre = tyre_type.TyreType(tyre_array)
+
+
+
+
+class TestCarriganTyre(unittest.TestCase):
+    def test_tyre_should_be_serviced(self):
+        tyre_wear_array = [0, 0.2, 0.03, 0.98]
+        tyre = Carrigan_tyre.CarriganTyre(tyre_wear_array)
         self.assertTrue(tyre.needs_service())
+        
+        
+    def test_tyre_should_not_be_serviced(self):
+        tyre_wear_array = [0, 0.2, 0.03, 0.4]
+        tyre = Carrigan_tyre.CarriganTyre(tyre_wear_array)
+        self.assertFalse(tyre.needs_service())
+        
+        
+        
+        
+class TestOctoprimeTyre(unittest.TestCase):
+    def test_tyre_should_be_serviced(self):
+        tyre_wear_array = [0.95, 0.95, 0.98, 0.98]
+        tyre = Octoprime_tyre.OctoprimeTyre(tyre_wear_array)
+        self.assertTrue(tyre.needs_service())
+    
+    
+    def test_tyre_should_not_be_serviced(self):
+        tyre_wear_array = [0, 0.2, 0.03, 0.98]
+        tyre = Octoprime_tyre.OctoprimeTyre(tyre_wear_array)
+        self.assertFalse(tyre.needs_service())
         
         
         
